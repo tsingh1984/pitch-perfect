4ce4001d40cc2441c4b5a2c88d4b18000dff075b
tsingh1984 <tsingh@grg.com.hk>
2015-03-17 16:17:19 +0800

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Tanvir Singh on 14/3/15.
+//  Copyright (c) 2015 Tanvir Singh. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
